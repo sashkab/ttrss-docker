132b9ecc5f9b7c785b93240b258c4a0d0eea4189
Aleks Bunin <github@compuix.com>
2017-11-04 00:03:08 -0400

    Backup job.

---
+++ backup.sh
@@ -0,0 +1,15 @@
+#/!/bin/env bash
+
+set -ex
+
+backup_path="${HOME}/.backup"
+mkdir -p "${backup_path}"
+fname="${backup_path}/ttrss-$(date +%Y.%m.%d).gz"
+
+docker exec -i ttrssdocker_postgres_1 pg_dump -U ttrss | gzip > "${fname}"
+
+if [ $? -eq 0 ]; then
+    echo "succsess"
+else
+    echo "failure"
+fi
