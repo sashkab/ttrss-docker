f31f3356961674f2ba93b42138ec2378b0f001c7
Aleks Bunin <github@compuix.com>
2024-08-06 21:19:04 -0400

    rename container name

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -ex
 
-while [ ! "$(docker container inspect -f '{{.State.Running}}' ttrssdocker_phpfpm_1)" == "true" ]; do
+while [ ! "$(docker container inspect -f '{{.State.Running}}' ttrssdocker-phpfpm-1)" == "true" ]; do
     echo "waiting for container to start..."
     sleep 5
 done
 
-docker exec -i ttrssdocker_phpfpm_1 bash -c "while [ ! -e /app/.ready ]; do echo waiting for app container...; sleep 10; done"
-docker exec -i ttrssdocker_phpfpm_1 bash -c "cd /app; sudo -E -u ttrss /usr/bin/php82 ./update.php --update-schema=force-yes"
+docker exec -i ttrssdocker-phpfpm-1 bash -c "while [ ! -e /app/.ready ]; do echo waiting for app container...; sleep 10; done"
+docker exec -i ttrssdocker-phpfpm-1 bash -c "cd /app; sudo -E -u ttrss /usr/bin/php82 ./update.php --update-schema=force-yes"
